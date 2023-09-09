b45564a808cd533d6282d1006ce61842dd2e1dd2
ZhangJian He <shoothzj@gmail.com>
2023-09-09 13:45:20 +0800

    Fix port pass bug (#5)

--- embedded-ignite-core/src/main/java/io/github/embedded/ignite/core/SocketUtil.java
+++ embedded-ignite-core/src/main/java/io/github/embedded/ignite/core/SocketUtil.java
@@ -2,6 +2,8 @@ package io.github.embedded.ignite.core;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SocketUtil {
 
@@ -9,7 +11,15 @@ public class SocketUtil {
         return getFreePorts(number, 10000, 49151);
     }
 
+    public static int[] getFreeServerPorts(int number, Set<Integer> exclusivePorts) {
+        return getFreePorts(number, 10000, 49151, exclusivePorts);
+    }
+
     public static int[] getFreePorts(int number, int start, int end) {
+        return getFreePorts(number, start, end, new HashSet<>());
+    }
+
+    public static int[] getFreePorts(int number, int start, int end, Set<Integer> exclusivePorts) {
         if (start < 0 || end > 65535 || start > end) {
             throw new IllegalArgumentException("Invalid port range");
         }
@@ -20,6 +30,10 @@ public class SocketUtil {
 
         try {
             for (int i = start; i <= end && found < number; i++) {
+                if (exclusivePorts.contains(i)) {
+                    continue;
+                }
+
                 try {
                     sockets[found] = new ServerSocket(i);
                     ports[found] = sockets[found].getLocalPort();
--- embedded-ignite-core/src/test/java/io/github/embedded/ignite/core/EmbeddedIgniteServerTest.java
+++ embedded-ignite-core/src/test/java/io/github/embedded/ignite/core/EmbeddedIgniteServerTest.java
@@ -13,4 +13,43 @@ public class EmbeddedIgniteServerTest {
         Assertions.assertTrue(server.httpPort() > 0);
         server.close();
     }
+
+    @Test
+    public void testIgniteServerBootSpecify1Port() throws Exception {
+        int[] serverPorts = SocketUtil.getFreeServerPorts(1);
+        EmbeddedIgniteConfig embeddedIgniteConfig = new EmbeddedIgniteConfig();
+        embeddedIgniteConfig.setClientConnectorPort(serverPorts[0]);
+        EmbeddedIgniteServer server = new EmbeddedIgniteServer(embeddedIgniteConfig);
+        server.start();
+        Assertions.assertEquals(serverPorts[0], server.clientConnectorPort());
+        server.close();
+    }
+
+    @Test
+    public void testIgniteServerBootSpecify2Port() throws Exception {
+        int[] serverPorts = SocketUtil.getFreeServerPorts(2);
+        EmbeddedIgniteConfig embeddedIgniteConfig = new EmbeddedIgniteConfig();
+        embeddedIgniteConfig.setClientConnectorPort(serverPorts[0]);
+        embeddedIgniteConfig.setJdbcPort(serverPorts[1]);
+        EmbeddedIgniteServer server = new EmbeddedIgniteServer(embeddedIgniteConfig);
+        server.start();
+        Assertions.assertEquals(serverPorts[0], server.clientConnectorPort());
+        Assertions.assertEquals(serverPorts[1], server.jdbcPort());
+        server.close();
+    }
+
+    @Test
+    public void testIgniteServerBootSpecify3Port() throws Exception {
+        int[] serverPorts = SocketUtil.getFreeServerPorts(3);
+        EmbeddedIgniteConfig embeddedIgniteConfig = new EmbeddedIgniteConfig();
+        embeddedIgniteConfig.setClientConnectorPort(serverPorts[0]);
+        embeddedIgniteConfig.setJdbcPort(serverPorts[1]);
+        embeddedIgniteConfig.setHttpPort(serverPorts[2]);
+        EmbeddedIgniteServer server = new EmbeddedIgniteServer(embeddedIgniteConfig);
+        server.start();
+        Assertions.assertEquals(serverPorts[0], server.clientConnectorPort());
+        Assertions.assertEquals(serverPorts[1], server.jdbcPort());
+        Assertions.assertEquals(serverPorts[2], server.httpPort());
+        server.close();
+    }
 }
